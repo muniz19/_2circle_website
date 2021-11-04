f76ba3082682eed4dde2f601751af271446fe004
muniz19 <munizvinicius861@gmail.com>
2021-11-03 22:46:37 -0300

    iniciando cadastro

---
+++ form-cadastro.php
@@ -0,0 +1,36 @@
+<?php
+include 'conexao.php';
+
+$name = $_POST['Nome'];
+$name = $_POST['Sobrenome'];
+$email = $_POST['Email'];
+$senha = $_POST['Senha'];
+$senha = $_POST['Confirme sua Senha'];
+$date = $_POST['Idade'];
+$tel = $_POST['Telefone'];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
