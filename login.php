7389e8cce4bac76ca3d080ce30a683efb0e54753
muniz19 <munizvinicius861@gmail.com>
2021-11-05 22:36:56 -0300

    inicio login

---
+++ login.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+</head>
+<body>
+    <h1>Login de usuário</h1>
+    <form action="ponte-login.php" method="post">
+
+    email: <input type="email" name="email"><br>
+    senha: <input type="password" name="senha"><br>
+
+    <input type="submit" value="entrar"><br>
+</form>
+</body>
+    </html>
---
+++ ponte-login.php
@@ -0,0 +1,19 @@
+<?php
+$email=$_POST["email"];
+$senha=$_POST["senha"];
+
+if(
+    (isset($email)&&isset($senha))
+    &&
+    (($email!=""&&($senha!=""))
+
+)
+{
+
+
+
+
+
+
+
+?>
